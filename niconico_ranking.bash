8b1b6dd24b13df27845035955fd9deecfa9d7d3d
gg_hatano <adad0405@gmail.com>
2014-11-11 23:31:27 +0900

    動画id入れました

--- niconico_ranking.bash
+++ niconico_ranking.bash
@@ -2,7 +2,8 @@
 
 dir=$(dirname $0)
 
-for genre in anime cooking dance game play radio sing sport vocaloid
+# for genre in anime cooking dance game play radio sing sport vocaloid
+for genre in anime 
 do
   echo $genre
   rankingUrl="http://www.nicovideo.jp/ranking/view/daily/$genre"
@@ -14,20 +15,19 @@ do
   cat $dir/tmp/ranking_${dateNow}_${genre} | 
   grep -A $length 'videoRanking' | 
   grep -B $length 'column sub' | 
-  grep -E '(count view|count comment|"rankingNum"|title)' | 
-  sed 's/<[^>]*>/ /g'  | 
-  grep -v "data-href" | 
+  grep -E '(count view|count comment|"rankingNum"|title|data-id=")' |
   sed 's/^ *//' | 
-  sed -e 's/再生//' -e "s/コメ//" |
-  sed 's/\([0-9]\),\([0-9]\)/\1\2/' |
-  sed 's/,/::/g' |
-  awk -v OFS="," '{print int(NR%4),$0}' | 
-  sed -e "s/^1/rank/" -e "s/^2/title/" -e "s/^3/view/" -e "s/^0/comment/" | 
-  tr '\n' ',' | 
-  gsed 's/rank/\nrank/g' | 
-  sed -e 's/,title,/,/' -e "s/rank,//" -e "s/,view,/,/" -e "s/comment,//" -e "s/,$//" | 
-  sed -e 's/ *,/,/g' -e 's/, */,/g' |
-  grep -v "^\s*$" > $dir/log/ranking_${dateNow}_${genre}
+  grep -v "data-enable-uad" | 
+  sed -e 's;<[^>]*>;;g' -e 's/\([0-9]\),\([0-9]\)/\1\2/g' | 
+  sed -e 's/再生//' -e 's/コメ//g' | 
+  grep -v "data-href" |
+  sed 's;.*/watch/\(..[0-9]*\)".*;\1;' | 
+  awk -v OFS="," '{print NR % 5, $0}' |
+  sed -e 's/^1/rank/' -e 's/^[0234]//' | 
+  tr '\n' ' ' | 
+  gsed 's/ rank/\nrank/g' | 
+  sed 's/^rank,//' | 
+  sed 's/ ,/,/g'> $dir/log/ranking_${dateNow}_${genre}
 
   sleep 1
 done
